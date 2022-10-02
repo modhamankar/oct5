c45a32479a512c4727c063b0133cc13ec8328f7a
mohitdhamankarr <43979624+mohitdhamankarr@users.noreply.github.com>
2022-10-02 17:47:10 +0530

    Update largest.py
    append function was missing.

--- largest.py
+++ largest.py
@@ -10,7 +10,7 @@ num = int(input("Enter number of elements in list: "))
 # iterating till num to append elements in list
 for i in range(1, num + 1):
 	ele = int(input("Enter elements: "))
-	list1(ele)
+	list1.append(ele)
 	
 # print maximum element
 print("Largest element is:", max(list1))
